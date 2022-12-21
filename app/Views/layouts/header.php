3a1a2c24bd652b7cb2e77a8e4726bde4c036b49a
miscapu <mignan2010@outlook.com>
2022-12-21 00:13:28 -0200

    Create Views files and requires autoload initial

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,22 @@
+<?php
+// define url root to my system crud
+define( 'base_url', 'http://localhost:8888/PHP/Crud2/' );
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= base_url.'assets/css/bootstrap.min.css'?>">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+        </div>
+        <div class="col">
